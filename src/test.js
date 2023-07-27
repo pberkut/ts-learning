bf9e81b42065317ffedb3465a6a7e0bb609c9acf
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-07-27 10:22:18 +0300

    update

--- src/test.js
+++
@@ -1,6 +0,0 @@
-function add(num1, num2) {
-  return num1 + num2;
-}
-const res = add(1, 1);
-
-console.warn(res);
---
+++ src/ts/test.js
@@ -0,0 +1,5 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(21, 1);
+console.log(result);
